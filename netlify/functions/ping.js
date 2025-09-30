cf151c282744c0113633628277f6e9ed4497f531
mohitsbh <msbhosale003@gmail.com>
2025-09-30 20:55:14 +0530

    update

--- netlify/functions/ping.js
+++
@@ -1,4 +0,0 @@
-exports.handler = async function (event, context) {
-  if (event.httpMethod === 'GET') return { statusCode: 200, body: 'pong' };
-  return { statusCode: 200, body: JSON.stringify({ ok: true, method: event.httpMethod }) };
-};
